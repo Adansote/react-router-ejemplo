c3d256e798e7e3d1fff9b3ceedfd7e4641751902
Adansote <adansotmx@gmail.com>
2023-04-11 10:41:54 -0600

    logo vite

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div className="navbar navbar-dark bg-dark  ">
 
             <div className="container d-flex justify-content-start  ">
-            <img  className="p-2 m-2" src="/public/vite.svg"  alt="" />
+            <img  className="p-2 m-2" src="/vite.svg"  alt="" />
             <NavLink className="btn btn-outline-primary p-2 m-2 " to="/">Inicio</NavLink>
             <NavLink className="btn btn-outline-primary p-2 m-2 " to="/contacto">Contacto</NavLink>
             <NavLink className="btn btn-outline-primary p-2 m-2 " to="/blog">Blog</NavLink>
